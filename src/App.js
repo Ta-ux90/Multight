f55e7e053695ebb0547dc506ce2371a93427aeff
Ta-ux90 <tayyabatarq11@gmail.com>
2025-11-10 18:18:02 +0500

    Deploy update: Home page fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import AppBar from './Components/AppBar';
 import Home from './Components/Home';
 import Blog from './Components/Blog';
